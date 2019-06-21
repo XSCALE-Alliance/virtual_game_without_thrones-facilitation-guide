0599465fc20ec8e2fb42c462c4ad35e03a1d1fc8
Marijn van der Zee <marijn@serraict.com>
2019-06-21 13:28:30 +0200

    Add shebang and make these files executable

--- scripts/install_gwt.sh
+++ scripts/install_gwt.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 git clone https://github.com/XSCALE-Alliance/virtual_game_without_thrones.git /usr/share/minetest/games/virtual_game_without_thrones
--- scripts/update_gwt.sh
+++ scripts/update_gwt.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 systemctl stop minetestrunner
 git -C /usr/share/minetest/games/virtual_game_without_thrones/ pull origin --recurse-submodules
 systemctl start minetestrunner
